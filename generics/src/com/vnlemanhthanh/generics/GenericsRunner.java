47e11f7a00d75c8cfaf7947470c7e01853298f75
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-11-26 22:10:45 +0700

    generics

--- generics/src/com/vnlemanhthanh/generics/GenericsRunner.java
+++ generics/src/com/vnlemanhthanh/generics/GenericsRunner.java
@@ -6,9 +6,15 @@ package com.vnlemanhthanh.generics;
 
 public class GenericsRunner {
     public static void main(String[] args) {
-        MyCustomList list = new MyCustomList();
+        MyCustomList<String> list = new MyCustomList<>();
         list.addElement("Element 1");
         list.addElement("Element 2");
+        System.out.println(list);
+
+        MyCustomList<Integer> list2 = new MyCustomList<>();
+        list2.addElement(5);
+        list2.addElement(7);
+        System.out.println(list2);
 
     }
 }
--- generics/src/com/vnlemanhthanh/generics/MyCustomList.java
+++ generics/src/com/vnlemanhthanh/generics/MyCustomList.java
@@ -6,14 +6,18 @@ package com.vnlemanhthanh.generics;
 
 import java.util.ArrayList;
 
-public class MyCustomList {
-    ArrayList<String> list = new ArrayList<>();
+public class MyCustomList<T> {
+    ArrayList<T> list = new ArrayList<>();
 
-    public void addElement(String element   ) {
+    public void addElement(T element   ) {
         list.add(element);
     }
 
-    public void  removeElement(String element) {
+    public void  removeElement(T element) {
         list.remove(element);
+    }
+
+    public String toString() {
+        return list.toString();
     }
 }
