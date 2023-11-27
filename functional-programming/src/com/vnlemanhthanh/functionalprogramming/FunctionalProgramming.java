d7bc1f6d85f72b6826ad70aeb6f2861782f7267a
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-11-27 10:08:20 +0700

    Filtering

--- functional-programming/src/com/vnlemanhthanh/functionalprogramming/FunctionalProgramming.java
+++ functional-programming/src/com/vnlemanhthanh/functionalprogramming/FunctionalProgramming.java
@@ -8,20 +8,46 @@ import java.util.List;
 
 public class FunctionalProgramming {
     public static void main(String[] args) {
-        List<String> list = List.of("Apple", "Banana", "Cat", "Dog");
-        // printBasic(list);
-        printWithFP(list);
+        List<String> list = List.of("Apple", "Bat", "Cat", "Dog");
+        printBasic(list);
+        // printWithFP(list);
+        printWithFPWithFiltering(list);
+
+        List<Integer> numbers = List.of(0,1,2,3,4,5,6);
+        filterList(numbers);
     }
 
     private static void printBasic(List<String> list) {
         for (String string : list) {
-            System.out.println(string);
+            if (string.endsWith("at")) {
+                System.out.println(string);
+            }
         }
+        System.out.println(">>>Done!\n");
     }
 
     private static void printWithFP(List<String> list) {
         list.stream().forEach(
                 element -> System.out.println(element)
             );
+    }
+
+    private static void printWithFPWithFiltering(List<String> list) {
+        list.stream()
+                .filter(e -> e.endsWith("at"))
+                .forEach(element -> System.out.println("element - " + element));
+    }
+
+    private static List filterList(List<Integer> list) {
+        list.stream()
+                .filter(e -> e % 2 == 0)
+                .forEach(e -> System.out.println("Even number: " +e));
+
+        list.stream()
+                .filter(e -> e % 2 == 1)
+                .forEach(e -> System.out.println("Odd number: " + e));
+
+        System.out.println(list);
+        return list;
     }
 }
