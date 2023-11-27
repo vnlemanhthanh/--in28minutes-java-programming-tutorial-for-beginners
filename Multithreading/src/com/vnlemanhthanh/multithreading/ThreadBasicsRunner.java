02837c3ba6e752f1bb1473185af68967aa8061e0
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-11-28 00:40:18 +0700

    Creating a Thread for Task2 - Implement Runnable Interface

--- Multithreading/src/com/vnlemanhthanh/multithreading/ThreadBasicsRunner.java
+++ Multithreading/src/com/vnlemanhthanh/multithreading/ThreadBasicsRunner.java
@@ -16,17 +16,26 @@ class Task1 extends Thread {
 }
 
 // implements Runnable
+class Task2 implements Runnable {
+
+    @Override
+    public void run() {
+        System.out.println("Task2 Started");
+        for (int i = 201; i <= 299 ; i++) {
+            System.out.printf("%s", i + ((299 != i) ? ", " : "\n"));
+        }
+        System.out.println("Task2 Done!");
+    }
+}
 
 public class ThreadBasicsRunner {
     public static void main(String[] args) {
         Task1 task1 = new Task1();
         task1.start();
 
-        System.out.println("Task2 Started");
-        for (int i = 201; i <= 299 ; i++) {
-            System.out.printf("%s", i + ((299 != i) ? ", " : "\n"));
-        }
-        System.out.println("Task2 Done!");
+        Task2 task2 = new Task2();
+        Thread task2Thread = new Thread(task2);
+        task2Thread.start();
 
         System.out.println("Task3 Started");
         for (int i = 301; i <= 399 ; i++) {
