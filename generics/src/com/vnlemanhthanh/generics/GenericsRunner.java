5e230e5e3e70e2dd43f5db6031488707f91bb3a6
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-11-27 08:26:07 +0700

    Restrictions with extends and Generic Methods

--- generics/src/com/vnlemanhthanh/generics/GenericsRunner.java
+++ generics/src/com/vnlemanhthanh/generics/GenericsRunner.java
@@ -4,8 +4,30 @@
 
 package com.vnlemanhthanh.generics;
 
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 public class GenericsRunner {
+
+    static <X> X doubleValue(X value) {
+        return value;
+    }
+
+    static <X extends List> void duplicate(X list) {
+        list.addAll(list);
+    }
+
     public static void main(String[] args) {
+
+        String value1 = doubleValue(new String());
+        Integer number1 = doubleValue(Integer.valueOf(5));
+        ArrayList<Object> list1 = doubleValue(new ArrayList<>());
+
+        LinkedList<Integer> numbers = new LinkedList<>(List.of(1,2,3));
+        duplicate(numbers);
+        System.out.println(numbers);
+
         MyCustomList<String> list = new MyCustomList<>();
         list.addElement("Element 1");
         list.addElement("Element 2");
