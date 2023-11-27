86fbacf302828f946636931a35224fe85e9118b0
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-11-27 09:40:32 +0700

    Function as Parameter

--- functional-programming/src/com/vnlemanhthanh/functionalprogramming/FunctionalProgramming.java
+++ functional-programming/src/com/vnlemanhthanh/functionalprogramming/FunctionalProgramming.java
@@ -4,6 +4,24 @@
 
 package com.vnlemanhthanh.functionalprogramming;
 
-public class FunctionalProgramming {
+import java.util.List;
 
+public class FunctionalProgramming {
+    public static void main(String[] args) {
+        List<String> list = List.of("Apple", "Banana", "Cat", "Dog");
+        // printBasic(list);
+        printWithFP(list);
+    }
+
+    private static void printBasic(List<String> list) {
+        for (String string : list) {
+            System.out.println(string);
+        }
+    }
+
+    private static void printWithFP(List<String> list) {
+        list.stream().forEach(
+                element -> System.out.println(element)
+            );
+    }
 }
