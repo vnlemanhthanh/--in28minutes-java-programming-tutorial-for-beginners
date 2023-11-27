161def16518ee8ea3f46f49f24370d18b263b3c5
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-11-27 16:04:56 +0700

    Behind the Screens with Functional Interfaces - Implement Consumer Int

--- functional-programming/src/com/vnlemanhthanh/functionalprogramming/LambdaBehindTheScreenRunner.java
+++ functional-programming/src/com/vnlemanhthanh/functionalprogramming/LambdaBehindTheScreenRunner.java
@@ -5,6 +5,7 @@
 package com.vnlemanhthanh.functionalprogramming;
 
 import java.util.List;
+import java.util.function.Consumer;
 import java.util.function.Predicate;
 
 class EvenNumberPredicate implements Predicate<Integer> {
@@ -15,21 +16,33 @@ class EvenNumberPredicate implements Predicate<Integer> {
     }
 }
 
+class SystemOutConsumer implements Consumer<Integer> {
+
+    @Override
+    public void accept(Integer number) {
+        System.out.println(number);
+    }
+}
+
 public class LambdaBehindTheScreenRunner {
     public static void main(String[] args) {
         List.of(34,43,34,45,36,48).stream()
                 .filter(new EvenNumberPredicate())
-                .forEach(e -> System.out.println(e));
+                .forEach(new SystemOutConsumer());
 
         System.out.println();
 
-        List.of(23,43,34,45,36,48).stream()
+        List.of(34,43,34,45,36,48).stream()
                 .filter(n -> n%2 == 0)
                 .forEach(e -> System.out.println(e));
 
+        // .filter(new EvenNumberPredicate())
         // Stream<T> filter(Predicate<? super T> predicate);
         // boolean test(T t);
 
+        // .forEach(e -> System.out.println(e));
+        // void forEach(Consumer<? super T> action);
+        // void accept(T t);
     }
 
 }
