79122cf47aabedb924648f131bcb2fee4abd4a15
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-11-28 15:10:52 +0700

    Implementing Auto Wiring in Spring Framework Java Configuration File

--- learn-spring-framework/src/main/java/com/vnlemanhthanh/learnspringframework/HelloWorldConfiguration.java
+++ learn-spring-framework/src/main/java/com/vnlemanhthanh/learnspringframework/HelloWorldConfiguration.java
@@ -7,9 +7,9 @@ package com.vnlemanhthanh.learnspringframework;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
-record Person(String name, int age) {}
-
 record Address(String firstLine, String city) {}
+
+record Person(String name, int age, Address address) {}
 
 @Configuration
 public class HelloWorldConfiguration {
@@ -24,14 +24,33 @@ public class HelloWorldConfiguration {
         return 15;
     }
 
-    @Bean
-    public Person person() {
-        return new Person("Ravi", 20);
-    }
-
-    @Bean
+    @Bean(name = "address2")
     public Address address() {
         return new Address("Baker Street", "London");
     }
 
+    @Bean
+    public Person person() {
+        return new Person("Ravi", 20, new Address("Main Street", "Utrecht"));
+    }
+
+    @Bean
+    public Person person2MethodCall() {
+        return new Person(name(), age(), address());
+    }
+
+    @Bean
+    public Person person3Parameters(String name, int age, Address address2) {
+        return new Person(name, age, address2);
+    }
+
+    @Bean(name = "address3")
+    public Address addres3() {
+        return new Address("Motinagar Street", "Hyderrabad");
+    }
+
+    @Bean
+    public Person person3Params(String name, int age, Address address3) {
+        return new Person(name, age, address3);
+    }
 }
