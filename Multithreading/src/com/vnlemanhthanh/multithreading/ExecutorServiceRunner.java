1673a04cbcb310e854da03d618ae012fe3dce842
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-11-28 08:51:36 +0700

    Executor Service - Customizing number of Threads

--- Multithreading/src/com/vnlemanhthanh/multithreading/ExecutorServiceRunner.java
+++ Multithreading/src/com/vnlemanhthanh/multithreading/ExecutorServiceRunner.java
@@ -7,20 +7,37 @@ package com.vnlemanhthanh.multithreading;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+class  Task extends Thread {
+    private int number;
+
+    public Task(int number) {
+        this.number = number;
+    }
+
+    public void run() {
+        System.out.printf("\nTask %d Started\n", number);
+        for (int i = 100 * number; i <= 99 + 100*number ; i++) {
+            System.out.printf("%s", i + (((99 + 100*number) != i) ? ", " : " "));
+        }
+        System.out.printf("\nTask %d Done!\n", number);
+    }
+}
+
 public class ExecutorServiceRunner {
     public static void main(String[] args) {
-        ExecutorService executorService = Executors.newSingleThreadExecutor();
-        executorService.execute(new Task1());
-        executorService.execute(new Thread(new Task2()));
-
-        System.out.println("Task3 Started");
-        for (int i = 301; i <= 399 ; i++) {
-            System.out.printf("%s", i + ((399 != i) ? ", " : "\n"));
-        }
-        System.out.println("Task3 Done!");
-
-        System.out.println("Main Done!");
+        ExecutorService executorService = Executors.newFixedThreadPool(5);
+        executorService.execute(new Task(111));
+        executorService.execute(new Task(222));
+        executorService.execute(new Task(333));
+        executorService.execute(new Task(444));
+        executorService.execute(new Task(555));
+        executorService.execute(new Task(666));
+        executorService.execute(new Task(777));
+        executorService.execute(new Task(888));
+        executorService.execute(new Task(999));
 
         executorService.shutdown();
     }
+
+
 }
