8e85eac966f5cd970d5323efa227a8474d2f8875
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-11-28 09:12:29 +0700

    Getting Started with Java Spring Framework

---
+++ learn-spring-framework/src/main/java/com/vnlemanhthanh/learnspringframework/LearnSpringFrameworkApplication.java
@@ -0,0 +1,13 @@
+package com.vnlemanhthanh.learnspringframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnSpringFrameworkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnSpringFrameworkApplication.class, args);
+    }
+
+}
