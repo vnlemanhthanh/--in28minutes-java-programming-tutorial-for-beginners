d4eac16a30d0fe2810e0421bbf9e53724b96d9bb
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-11-29 15:19:07 +0700

    Implementing Comparable Inteface

--- collections/src/com/vnlemanhthanh/collections/StudentsCollectionsRunner.java
+++ collections/src/com/vnlemanhthanh/collections/StudentsCollectionsRunner.java
@@ -4,8 +4,25 @@
 
 package com.vnlemanhthanh.collections;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class StudentsCollectionsRunner {
     public static void main(String[] args) {
+
+        List<Student> students =
+                List.of(new Student(1, "Thanh"),
+                new Student(100, "Adam"),
+                new Student(3, "Eve"));
+
+        List<Student> studentsAl = new ArrayList<Student>(students);
+
+        System.out.println(students);
+        System.out.println(studentsAl);
+
+        Collections.sort(studentsAl);
+        System.out.println(studentsAl);
 
     }
 
