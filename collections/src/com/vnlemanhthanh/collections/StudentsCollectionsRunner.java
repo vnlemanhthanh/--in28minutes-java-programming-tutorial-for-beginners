46435ac78ab093074b565b922ccf07cc56f70768
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-11-29 14:43:29 +0700

    Introduction to Collections sort static

---
+++ collections/src/com/vnlemanhthanh/collections/StudentsCollectionsRunner.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2023. vnlemanhthanh.com
+ */
+
+package com.vnlemanhthanh.collections;
+
+public class StudentsCollectionsRunner {
+    public static void main(String[] args) {
+
+    }
+
+}
