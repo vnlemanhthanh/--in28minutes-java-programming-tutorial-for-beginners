6b26e91368f6b1f18a9f2ccd7de89249aab68d07
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-11-29 23:28:44 +0700

    Book class and Three instances

---
+++ introduction-to-OOP/src/com/vnlemanhthanh/oops/Book.java
@@ -0,0 +1,8 @@
+/*
+ * Copyright (c) 2023. vnlemanhthanh.com
+ */
+
+package com.vnlemanhthanh.oops;
+
+public class Book {
+}
---
+++ introduction-to-OOP/src/com/vnlemanhthanh/oops/BookRunner.java
@@ -0,0 +1,14 @@
+/*
+ * Copyright (c) 2023. vnlemanhthanh.com
+ */
+
+package com.vnlemanhthanh.oops;
+
+public class BookRunner    {
+    public static void main(String[] args) {
+        Book artOfComputerProgramming = new Book();
+        Book effectiveJava = new Book();
+        Book cleanCode = new Book();
+
+    }
+}
