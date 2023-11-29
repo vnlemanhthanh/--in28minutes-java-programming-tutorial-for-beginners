041c9b9cac67bc63e3011ca0e64700040a4cdf37
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-11-29 23:24:37 +0700

    OOP

---
+++ introduction-to-OOP/src/com/vnlemanhthanh/oops/MotorBike.java
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2023. vnlemanhthanh.com
+ */
+
+package com.vnlemanhthanh.oops;
+
+public class MotorBike {
+    void start() {
+        System.out.println("Motor Bike");
+    }
+}
---
+++ introduction-to-OOP/src/com/vnlemanhthanh/oops/MotorBikeRunner.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (c) 2023. vnlemanhthanh.com
+ */
+
+package com.vnlemanhthanh.oops;
+
+public class MotorBikeRunner {
+    public static void main(String[] args) {
+
+        MotorBike ducati = new MotorBike();
+        MotorBike honda = new MotorBike();
+
+        ducati.start();
+        honda.start();
+    }
+}
