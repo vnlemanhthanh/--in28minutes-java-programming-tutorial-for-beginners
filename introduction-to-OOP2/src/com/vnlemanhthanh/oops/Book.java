e8a2fced91674811b5fae2228b20a36869d2454c
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-11-30 22:40:50 +0700

    Understanding Object Composition. Close #218

---
+++ introduction-to-OOP2/src/com/vnlemanhthanh/oops/Book.java
@@ -0,0 +1,40 @@
+/*
+ * Copyright (c) 2023. vnlemanhthanh.com
+ */
+
+package com.vnlemanhthanh.oops;
+
+import java.util.ArrayList;
+
+public class Book {
+    private int id;
+    private String name;
+    private String author;
+    private ArrayList<Review> reviews = new ArrayList<>();
+
+    public Book(int id, String name, String author) {
+        this.id = id;
+        this.name = name;
+        this.author = author;
+    }
+
+    public void addReview(Review review) {
+        this.reviews.add(review);
+    }
+
+    public String toString() {
+        return "Book{" + "id=" + id + ", name='" + name + '\'' + ", author='" + author + '\'' + ", reviews=" + reviews + '}';
+    }
+
+    //Test Client
+    public static void main(String[] args) {
+        Book book = new Book(123, "Object Oriented Programming with Java", "Thanh");
+
+        book.addReview(new Review(10, "Greater Book", 5));
+        book.addReview(new Review(101, "Awesome Book",5));
+
+        System.out.println(book);
+
+
+    }
+}
---
+++ introduction-to-OOP2/src/com/vnlemanhthanh/oops/Review.java
@@ -0,0 +1,22 @@
+/*
+ * Copyright (c) 2023. vnlemanhthanh.com
+ */
+
+package com.vnlemanhthanh.oops;
+
+public class Review {
+    private int id;
+    private String description;
+    private int rating;
+
+    public Review(int id, String description, int rating) {
+        this.id = id;
+        this.description = description;
+        this.rating = rating;
+    }
+
+    @Override
+    public String toString() {
+        return "Review{" + "id=" + id + ", description='" + description + '\'' + ", rating=" + rating + '}';
+    }
+}
