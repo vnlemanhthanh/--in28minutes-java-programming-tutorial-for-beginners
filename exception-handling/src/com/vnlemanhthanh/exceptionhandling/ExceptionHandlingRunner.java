554fd64dcf8c3fd92941ccd525253686861e4fb9
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-12-01 14:56:40 +0700

    Basics of Exceptions - NullPointerException and StackTrace. Close 313

---
+++ exception-handling/src/com/vnlemanhthanh/exceptionhandling/ExceptionHandlingRunner.java
@@ -0,0 +1,23 @@
+/*
+ * Copyright (c) 2023. vnlemanhthanh.com
+ */
+
+package com.vnlemanhthanh.exceptionhandling;
+
+public class ExceptionHandlingRunner {
+    public static void main(String[] args) {
+        method1();
+        System.out.println("ExceptionHandlingRunner.main Ended");
+    }
+
+    private static void method1() {
+        method2();
+        System.out.println("ExceptionHandlingRunner.method1 Ended");
+    }
+
+    private static void method2() {
+        String str = null;
+        str.length();
+        System.out.println("ExceptionHandlingRunner.method2 Ended");
+    }
+}
