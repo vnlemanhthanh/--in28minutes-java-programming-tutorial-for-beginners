de0d8f099b5d76613e7d70c3c44ccd0ad23b78ed
vnlemanhthanh <vnlemanhthanh@gmail.com>
2023-12-02 09:10:18 +0700

    Files. Close #328

--- files/src/com/vnlemanhthanh/files/DirectoryScanRunner.java
+++ files/src/com/vnlemanhthanh/files/DirectoryScanRunner.java
@@ -4,8 +4,13 @@
 
 package com.vnlemanhthanh.files;
 
-public class DirectoryScanRunner {
-    public static void main(String[] args) {
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
 
+public class DirectoryScanRunner {
+    public static void main(String[] args) throws IOException {
+
+        Files.list(Paths.get(".")).forEach(System.out::println);
     }
 }
---
+++ tips/src/com/vnlemanhthanh/tips/Public.java
@@ -0,0 +1,13 @@
+/*
+ * Copyright (c) 2023. vnlemanhthanh.com
+ */
+
+package com.vnlemanhthanh.tips;
+
+
+public class Public {
+
+    public static void main(String[] args) {
+
+    }
+}
